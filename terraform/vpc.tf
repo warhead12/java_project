52d0f120ba74740e605d7eabc5d9d3797b602c98
Abhishek Anand <abhishekanandspass1@gmail.com>
2026-01-25 17:36:31 +0530

    public route table added

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -24,3 +24,16 @@ resource "aws_subnet" "private2" {
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
